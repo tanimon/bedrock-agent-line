86c2de030a7b6a35fc2bf58ff7a5b760c54d5d37
tanimon <8575113+tanimon@users.noreply.github.com>
2023-12-30 02:39:13 +0900

    Update to use LangChain Bedrock class instead of raw BedrockRuntimeClient

--- server/src/handler/bedrock-handler.ts
+++ server/src/handler/bedrock-handler.ts
@@ -1,38 +1,23 @@
-// eslint-disable-next-line strict-dependencies/strict-dependencies
-import {
-  BedrockRuntimeClient,
-  InvokeModelCommand,
-} from '@aws-sdk/client-bedrock-runtime';
 import { APIGatewayProxyHandler } from 'aws-lambda';
+import { Bedrock } from 'langchain/llms/bedrock';
 
-const client = new BedrockRuntimeClient({ region: 'us-east-1' });
+const llm = new Bedrock({
+  region: 'us-east-1',
+  model: 'amazon.titan-text-express-v1',
+  maxTokens: 4096,
+  temperature: 0,
+  verbose: true,
+});
 
 export const handler: APIGatewayProxyHandler = async (event) => {
   // eslint-disable-next-line no-console
   console.dir({ event }, { depth: null });
 
   const { query } = event as unknown as { query: string };
-  const requestBody = JSON.stringify({
-    inputText: query,
-    textGenerationConfig: {
-      maxTokenCount: 4096,
-      stopSequences: [],
-      temperature: 0,
-      topP: 1,
-    },
-  });
-  const response = await client.send(
-    new InvokeModelCommand({
-      modelId: 'amazon.titan-text-express-v1',
-      accept: 'application/json',
-      contentType: 'application/json',
-      body: requestBody,
-    })
-  );
-  const responseBody = response.body.transformToString();
+  const response = await llm.invoke(query);
 
   // eslint-disable-next-line no-console
-  console.dir({ responseBody }, { depth: null });
+  console.dir({ response }, { depth: null });
 
   return {
     statusCode: 200,

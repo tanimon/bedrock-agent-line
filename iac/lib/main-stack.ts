10efeff9d69b4e9be48e969b7d1f50dc4b114e7b
tanimon <8575113+tanimon@users.noreply.github.com>
2023-12-30 01:21:30 +0900

    Add Lambda function resource

--- iac/lib/main-stack.ts
+++ iac/lib/main-stack.ts
@@ -1,16 +1,32 @@
-import * as cdk from 'aws-cdk-lib';
+import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { Architecture, Runtime, Tracing } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class MainStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class MainStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'IacQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const fn = new NodejsFunction(this, 'BedrockFn', {
+      entry: '../server/src/handler/bedrock-handler.ts',
+      // TODO: extract default props
+      architecture: Architecture.ARM_64,
+      runtime: Runtime.NODEJS_20_X,
+      tracing: Tracing.ACTIVE,
+      timeout: Duration.seconds(30),
+      bundling: {
+        sourceMap: true,
+        // LambdaランタイムにはのBedrockのSDKが含まれていないようなので、AWS SDKをバンドルさせる
+        externalModules: [],
+      },
+    });
+    fn.addToRolePolicy(
+      new PolicyStatement({
+        effect: Effect.ALLOW,
+        actions: ['bedrock:*'],
+        resources: ['*'],
+      })
+    );
   }
 }
